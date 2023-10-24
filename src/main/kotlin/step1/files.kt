c7403479b6099f3641e830b06f82986ee1aeac7c
Alexandr Belyaev <eminis.ru@gmail.com>
2023-10-25 00:34:20 +0300

    Step1 - Create file with words

---
+++ src/main/kotlin/step1/files.kt
@@ -0,0 +1,11 @@
+package step1
+
+import java.io.File
+
+fun main() {
+
+    val wordsFile: File = File("words.txt")
+    wordsFile.createNewFile()
+
+    for (line in wordsFile.readLines()) println(line)
+}
