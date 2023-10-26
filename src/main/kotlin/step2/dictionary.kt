0c0e786fc869390849a54552422117f30e7f3153
Belyaev.AA4 <eminis.ru@gmail.com>
2023-10-26 12:41:29 +0300

    Step2 - Create dictionary (Edited)

--- src/main/kotlin/step2/dictionary.kt
+++ src/main/kotlin/step2/dictionary.kt
@@ -9,18 +9,14 @@ fun main() {
 
     for (line in wordsFile.readLines()) {
         val parsedLine = line.split("|")
-        dictionary.add(Word(parsedLine[0], parsedLine[1]))
+        dictionary.add(Word(parsedLine[0], parsedLine[1], parsedLine[2]?.toIntOrNull() ?: 0))
     }
 
     for (i in dictionary) println(i)
-
-    val numbersOfWords = File("numbersOfWords.txt")
-    val learnedWords = 32
-    numbersOfWords.writeText(learnedWords.toString())
 }
 
 data class Word(
     val text: String,
     val translate: String,
-    val numbersOfWords: File = File("numbersOfWords.txt")
+    val correctAnswersCount: Int?
 )
