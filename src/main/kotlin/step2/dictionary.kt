cc64cde2a34e8e7d2eddcf637652e6da8819d35d
Alexandr Belyaev <eminis.ru@gmail.com>
2023-10-27 22:41:51 +0300

    Step3 - Add MainMenu And Statistic

--- src/main/kotlin/step2/dictionary.kt
+++ src/main/kotlin/step2/dictionary.kt
@@ -3,16 +3,19 @@ package step2
 import java.io.File
 
 fun main() {
-    val wordsFile = File("words.txt")
+    val firstDictionary = Dictionary()
 
-    val dictionary: MutableList<Word> = mutableListOf()
+    firstDictionary.addToDictionary()
 
-    for (line in wordsFile.readLines()) {
-        val parsedLine = line.split("|")
-        dictionary.add(Word(parsedLine[0], parsedLine[1], parsedLine[2]?.toIntOrNull() ?: 0))
+    while (true) {
+        println("Меню: 1 – Учить слова, 2 – Статистика, 0 – Выход")
+        when (readln().toInt()) {
+            0 -> return
+            1 -> println("Учить слова")
+            2 -> println(firstDictionary.statistic())
+            else -> println("Введено неверное значение")
+        }
     }
-
-    for (i in dictionary) println(i)
 }
 
 data class Word(
@@ -20,3 +23,28 @@ data class Word(
     val translate: String,
     val correctAnswersCount: Int?
 )
+
+class Dictionary(
+    val dictionary: MutableList<Word> = mutableListOf()
+) {
+    fun addToDictionary() {
+        val wordsFile = File("words.txt")
+        for (line in wordsFile.readLines()) {
+            val parsedLine = line.split("|")
+            dictionary.add(Word(parsedLine[0], parsedLine[1], parsedLine[2]?.toIntOrNull() ?: 0))
+        }
+    }
+    fun printDictionary() {
+        for (i in dictionary) println(i)
+    }
+
+    fun statistic() {
+        val learnedWords = dictionary.filter { word: Word -> word.correctAnswersCount!! >= 3 }.size
+        val totalWords = dictionary.size
+        val percent:Double = (learnedWords.toDouble() / totalWords.toDouble()) * 100
+
+        println("Выучено $learnedWords из $totalWords слов | ${percent.toInt()}%")
+    }
+}
+
+
