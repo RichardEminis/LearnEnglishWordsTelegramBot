40440671b963d777cd8e91a293f87f19657a676d
Alexandr Belyaev <eminis.ru@gmail.com>
2023-11-13 00:41:16 +0300

    Step6 - Code refactoring

---
+++ src/main/kotlin/LearnWordsTrainer/LearnWordsTrainer.kt
@@ -0,0 +1,70 @@
+package LearnWordsTrainer
+
+import java.io.File
+
+data class Statistics(
+    val learnedWords: Int,
+    val totalWords: Int,
+    val percent: Double
+)
+
+data class Question(
+    var variants: List<Word>,
+    val correctAnswer: Word
+)
+
+class LearnWordsTrainer(
+    val dictionary: MutableList<Word> = mutableListOf(),
+    private var question: Question? = null
+) {
+
+    fun loadDictionary() {
+        val wordsFile = File("words.txt")
+        for (line in wordsFile.readLines()) {
+            val parsedLine = line.split("|")
+            dictionary.add(Word(parsedLine[0], parsedLine[1], parsedLine[2]?.toIntOrNull() ?: 0))
+        }
+    }
+
+    fun saveDictionary(dictionary: List<Word>) {
+        val wordsFile = File("words.txt")
+        var savedText = ""
+        for (i in dictionary) {
+            val line = "${i.text}|${i.translate}|${i.correctAnswersCount}\n"
+            savedText += line
+        }
+        wordsFile.writeText(savedText)
+    }
+
+    fun getStatistics(): Statistics {
+        val learnedWords =
+            dictionary.filter { word: Word -> word.correctAnswersCount >= MIN_CORRECT_ANSWERS_FOR_LEARNED }.size
+        val totalWords = dictionary.size
+        val percent: Double = (learnedWords.toDouble() / totalWords.toDouble()) * 100
+
+        return Statistics(learnedWords, totalWords, percent)
+    }
+
+    fun getNextQuestion(): Question? {
+        val unlearnedWords = dictionary.filter { it.correctAnswersCount < MIN_CORRECT_ANSWERS_FOR_LEARNED }
+        if (unlearnedWords.isEmpty()) return null
+        val displayedWords = unlearnedWords.shuffled().take(NUMBER_OF_DISPLAYED_WORDS)
+        val selectedWord = displayedWords.random()
+        question = Question(variants = displayedWords, correctAnswer = selectedWord)
+        return question
+    }
+
+    fun checkAnswer(userAnswerIndex: Int?): Boolean {
+        return question?.let {
+            if (it.variants.indexOf(it.correctAnswer) + 1 == userAnswerIndex) {
+                it.correctAnswer.correctAnswersCount++
+                saveDictionary(dictionary)
+                true
+            } else {
+                false
+            }
+        } ?: false
+    }
+}
+
+
---
+++ src/main/kotlin/LearnWordsTrainer/Main.kt
@@ -0,0 +1,73 @@
+package LearnWordsTrainer
+
+const val NUMBER_OF_DISPLAYED_WORDS = 4
+const val MIN_CORRECT_ANSWERS = 2
+const val MIN_CORRECT_ANSWERS_FOR_LEARNED = 3
+
+data class Word(
+    val text: String,
+    val translate: String,
+    var correctAnswersCount: Int = 0
+)
+
+fun Question.asConsoleString(): String {
+    val variants = this.variants
+        .mapIndexed { index, word -> "${index + 1}) ${word.translate}" }
+        .joinToString(
+            prefix = "Изучаемое слово: ${this.correctAnswer.text}\nВарианты ответа:\n",
+            separator = "\n",
+            postfix = "\nВведите вариант ответа от 1 до $NUMBER_OF_DISPLAYED_WORDS\nДля выхода введите '0'"
+        )
+    return variants
+}
+
+fun main() {
+    val trainer = LearnWordsTrainer()
+
+    trainer.loadDictionary()
+
+    while (true) {
+        println("Меню: 1 – Учить слова, 2 – Статистика, 0 – Выход")
+        when (readln().toIntOrNull()) {
+            0 -> return
+            1 -> {
+                while (true) {
+                    val question = trainer.getNextQuestion()
+                    if (question == null) {
+                        println("Вы выучили все слова")
+                        break
+                    }
+
+                    if (question.variants.size < NUMBER_OF_DISPLAYED_WORDS) {
+                        val learnedWords = trainer.dictionary
+                            .filter { word: Word -> word.correctAnswersCount > MIN_CORRECT_ANSWERS }
+                            .shuffled()
+                            .take(NUMBER_OF_DISPLAYED_WORDS - question.variants.size)
+                        question.variants = (question.variants + learnedWords).shuffled()
+                    }
+
+                    println(question.asConsoleString())
+
+                    val userAnswerInput = readln().toIntOrNull()
+                    if (userAnswerInput == 0) break
+
+                    if (trainer.checkAnswer(userAnswerInput)) {
+                        println("Верный ответ!\n")
+                    } else {
+                        println("Неверный ответ!\n")
+                    }
+                }
+            }
+
+            2 -> {
+                val statistics = trainer.getStatistics()
+                println("Выучено ${statistics.learnedWords} из ${statistics.totalWords} слов | ${statistics.percent}%")
+            }
+
+            else -> println("Введено неверное значение")
+        }
+    }
+}
+
+
+
