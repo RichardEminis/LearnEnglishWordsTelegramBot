e6d5e50e41881f13f77e5fb2f3b95bedd6eb7482
Alexandr Belyaev <eminis.ru@gmail.com>
2023-11-16 09:15:02 +0300

    Step7 - Create class Telegram (Edited)

--- src/main/kotlin/LearnWordsTrainer/Telegram.kt
+++ src/main/kotlin/LearnWordsTrainer/Telegram.kt
@@ -8,13 +8,27 @@ import java.net.http.HttpResponse
 fun main(args: Array<String>) {
 
     val botToken = args[0]
-    val urlGetMe = "https://api.telegram.org/bot$botToken/getMe"
+    var updateId = 0
 
+    while (true) {
+        Thread.sleep(2000)
+        val updates: String = getUpdates(botToken, updateId)
+        println(updates)
+
+        val startUpdateId = updates.lastIndexOf("update_id")
+        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
+        if (startUpdateId == -1 || endUpdateId == -1) continue
+        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
+
+        updateId = updateIdString.toInt() + 1
+    }
+}
+
+fun getUpdates(botToken: String, updateId: Int): String {
+    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
     val client: HttpClient = HttpClient.newBuilder().build()
-
-    val requests: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetMe)).build()
-
+    val requests: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
     val response: HttpResponse<String> = client.send(requests, HttpResponse.BodyHandlers.ofString())
 
-    println(response.body())
+    return response.body()
 }
